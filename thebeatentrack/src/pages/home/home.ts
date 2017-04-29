f8c1ffa575ffa781a07a96dc12f96f6ffa629382
krzysztof.ignasiak <krzysztof.ignasiak@alertlogic.com>
2017-04-29 19:02:25 +0100

    Adding AR page, categories, home, locations, tabs, tours

--- thebeatentrack/src/pages/home/home.ts
+++ thebeatentrack/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LocationsPage } from "../locations/locations";
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  goToLocations() {
+    this.navCtrl.push(LocationsPage);
+  }
 }
