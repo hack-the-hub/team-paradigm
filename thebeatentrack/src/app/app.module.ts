7394cefca4f21fb0b8605a2a83781e54f7376f1a
Conal McLaughlin <conalsmclaughlin@gmail.com>
2017-04-29 13:23:12 +0100

    updating pages/tabs

--- thebeatentrack/src/app/app.module.ts
+++ thebeatentrack/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { BeatenTrackPage } from '../pages/beatentrack/beatentrack';
+import { ToursPage } from '../pages/tours/tours';
+import { InfoPage } from '../pages/info/info';
+import { MapPage } from '../pages/map/map';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -15,9 +15,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    BeatenTrackPage,
+    ToursPage,
+    InfoPage,
+    MapPage,
     HomePage,
     TabsPage
   ],
@@ -28,9 +28,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    BeatenTrackPage,
+    ToursPage,
+    InfoPage,
+    MapPage,
     HomePage,
     TabsPage
   ],
---
+++ thebeatentrack/src/pages/tours/tours.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-tours',
+  templateUrl: 'tours.html'
+})
+export class ToursPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
